46757ff1149c83ff9bd7eb428b93d12fb1c555a2
Jayden Skuladottir <sask5403@colorado.edu>
2023-09-21 15:36:44 -0600

    Update assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,19 @@
+let event1 = document.querySelector(".css-thru-js");
+event1.style.color = "red";
+event1.style.backgroundColor = "blue";
+
+function changeButton()
+{
+    button.textContent = "Hello!";
+}
+
+let button = document.querySelector(".button");
+button.addEventListener("click", changeButton);
+
+function changeColor()
+{
+    hover.style.color = "blue";
+}
+
+let hover = document.querySelector(".hover");
+button.addEventListener("mouseover", changeColor);
