c40f08d99603b8f3d49f22aee8eb387eb8a08bd6
Jayden Skuladottir <sask5403@colorado.edu>
2023-09-25 17:15:11 -0600

    Update assignment2.js

--- assignment2.js
+++ assignment2.js
@@ -6,3 +6,12 @@ function checkGuess()
 let guessButton = document.querySelector(".guessSubmit");
 let userGuess = document.querySelector(".guessField");
 guessButton.addEventListener("click", checkGuess);
+
+function checkDate()
+{
+    alert("Thanks for submitting a date");
+}
+
+let dateButton = document.querySelector(".dateSubmit");
+let dateInput = document.querySelector(".datetime");
+dateButton.addEventListener("click", checkDate);
