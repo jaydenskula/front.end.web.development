f4ce13c351c152f7fb15dac3b1bf493c40b85d3c
Jayden Skuladottir <sask5403@colorado.edu>
2023-09-26 10:38:18 -0600

    Update assignment2.js

--- assignment2.js
+++ assignment2.js
@@ -6,3 +6,8 @@ function checkGuess()
 let guessButton = document.querySelector(".guessSubmit");
 let userGuess = document.querySelector(".guessField");
 guessButton.addEventListener("click", checkGuess);
+
+let bgclr = document.getElementById("color");
+bgclr.addEventListener("input", () => {
+    document.body.style.backgroundColor = bgclr.value;
+})
