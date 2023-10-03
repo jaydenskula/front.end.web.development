083e375a45456182ece1b3a8ab64174ca6de231a
Jayden Skuladottir <sask5403@colorado.edu>
2023-10-02 18:15:31 -0600

    Update assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -1,33 +1,33 @@
 function quiz() {
-    const questions = [
-      'How many moons does Earth have?',
-      'How many moons does Saturn have?',
-      'How many moons does Venus have?'
-    ];
-    const answers = [1, 82, 0];
-  
-    let score = 0;
-  
-    let num = Math.floor(Math.random() * 1);
-  
-    for (let i = 0; i <= questions.count; i++) {
-      const question = questions[num];
-      let answer = prompt(question);
-      if (answer == answers[num]) {
-        score++;
-        alert('Correct!');
-      } else {
-        alert('Wrong!');
-      }
-      num++;
-      if (num === questions.length) {
-        num = 0;
-      }
+  const questions = [
+    'How many moons does Earth have?',
+    'How many moons does Saturn have?',
+    'How many moons does Venus have?'
+  ];
+  const answers = [1, 82, 0];
+
+  let score = 0;
+
+  let num = Math.floor(Math.random() * 1);
+
+  for (let i = 0; i <= questions.count; i++) {
+    const question = questions[num];
+    let answer = prompt(question);
+    if (answer == answers[num]) {
+      score++;
+      alert('Correct!');
+    } else {
+      alert('Wrong!');
     }
-  
-    const scoreArea = document.querySelector('score-area');
-    scoreArea.innerHTML = `You got ${score} out of ${questions.length} questions correct!`;
+    num++;
+    if (num === questions.length) {
+      num = 0;
+    }
   }
-  
-  alert('Are you ready for a quiz?');
-  quaz();
+
+  const scoreArea = document.querySelector('score-area');
+  scoreArea.innerHTML = `You got ${score} out of ${questions.length} questions correct!`;
+}
+
+alert('Are you ready for a quiz?');
+quiz();
