b044ad3271cf9b1669aed29c58b4a4c39f8f9d39
Jayden Skuladottir <sask5403@colorado.edu>
2023-10-07 19:20:20 -0600

    Update assignment4.js

--- assignment4.js
+++ assignment4.js
@@ -1,22 +1,23 @@
-// meowfacts API
-fetch('https://meowfacts.herokuapp.com/')
-  .then((response) => { return response.json() })
-  //.then(function(response) { return response.json() })
-  .then((data) => { console.log(data) });
+fetch('https://www.boredapi.com/api/activity')
+.then((response) => { return response.json() })
+//.then(function(response) { return response.json() })
+.then((data) => { console.log(data) });
 
-function writeMeowFactsToPage(json) {
-  for (const meowFact of json.data) {
-    const p = document.createElement('p');
-    p.innerText = meowFact;
-    document.body.appendChild(p);
-  }
-}
+fetch('https://www.boredapi.com/api/activity/')
+.then((response) => { return response.json() })
+.then((data) => {
+    // Get a reference to the HTML element where you want to display the data
+    const activityContainer = document.getElementById('activity-container');
 
-fetch('https://meowfacts.herokuapp.com/?count=3')
-  .then(response => response.json())
-  .then(writeMeowFactsToPage);
+    // Create a new HTML element to hold the data (e.g., a <p> element)
+    const activityParagraph = document.createElement('p');
 
-  fetch('https://www.boredapi.com/api/activity')
-  .then((response) => { return response.json() })
-  //.then(function(response) { return response.json() })
-  .then((data) => { console.log(data) });
+    // Set the text content of the new element to the fetched activity
+    activityParagraph.textContent = `Activity: ${data.activity}`;
+
+    // Append the new element to the container
+    activityContainer.appendChild(activityParagraph);
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
