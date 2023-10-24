b0ebf65eba5a87547b85423dbeec649b2fa27197
Jayden Skuladottir <sask5403@colorado.edu>
2023-10-24 16:26:50 -0600

    Update project1.js

--- project1/project1.js
+++ project1/project1.js
@@ -1,13 +1,51 @@
-var animation = anime.timeline({
+var start = anime.timeline({
     autoplay: false
 });
 
-animation
+start
 .add({
-    targets: '#btn',
-    top: '100px',
+    targets: '#start',
+    top: '-100%',
     duration: 500,
     easing: 'easeInOutSine'
 })
 
-document.querySelector('#btn').onclick = animation.play;
+.add({
+    targets: '#stars',
+    top: '0px',
+    duration: 1000,
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets: '#mountains_night',
+    bottom: '0px',
+    duration: 1000,
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets: '#moon',
+    top: '0px',
+    duration: 1000,
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets: '#fire',
+    top: '55%',
+    duration: 1000,
+    easing: 'easeInOutSine'
+})
+
+document.querySelector('#start').onclick = start.play;
+
+
+let gameText = document.querySelector("#game_text");
+
+function fire_text()
+{
+    gameText.textContent = "You light a fire";
+}
+
+fire.addEventListener("click", fire_text);
