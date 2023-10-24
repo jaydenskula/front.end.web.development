2bed9c5d0b8d2e6a3a0c43f83c4c2b860f5ba51c
Jayden Skuladottir <sask5403@colorado.edu>
2023-10-23 21:54:32 -0600

    Delete script2.js

--- assignment5/script2.js
+++
@@ -1,7 +0,0 @@
-const helloString = 'hello, my name is jayden!';
-const birthYear = 2001;
-function printTheYear() {
-  document.querySelector('p').innerText += year;
-}
-
-export { helloString as hello, birthYear, printTheYear };
