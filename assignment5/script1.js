17f3a5c0024a0378eff11b15c7a229d08cef5da3
Jayden Skuladottir <sask5403@colorado.edu>
2023-10-23 21:54:30 -0600

    Delete script1.js

--- assignment5/script1.js
+++
@@ -1,9 +0,0 @@
-// import { hello as helloStr, year, printTheYear } from "./script2.js";
-import * as script2Stuff from "./script2.js";
-import printTheMonth from "./script3.js";
-
-console.log(script2Stuff.hello);
-console.log(script2Stuff.year);
-script2Stuff.printTheYear();
-
-printMyMajor();
